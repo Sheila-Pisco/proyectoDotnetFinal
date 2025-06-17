c1bae0917d94746b300e3e248d0c0f17d1adfa3f
Sheila <spisco@fi.uba.ar>
2025-06-17 18:53:14 -0300

    cambios menores en Program y ListarEventos

--- CentroEventos/CentroEventos.UI/Program.cs
+++ CentroEventos/CentroEventos.UI/Program.cs
@@ -13,6 +13,7 @@ CentroEventosSqlite.Inicializar(); //solo tiene efecto si la base de datos no ex
 using var context = new CentroEventoContext();
 
 //Agrego algunos datos
+
 context.EventosDeportivos.Add(new EventoDeportivo("Aerobicos", "Movimiento", new DateTime(2025, 1, 1), 1.5, 15, 2));
 context.EventosDeportivos.Add(new EventoDeportivo("Zumba", "Baile", new DateTime(2025, 7, 7), 1.5, 25, 3));
 context.EventosDeportivos.Add(new EventoDeportivo("Funcional", "Desafío", new DateTime(2025, 8, 9), 1.5, 10, 4));
